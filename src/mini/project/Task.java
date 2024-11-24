811f0cb55a915f7ee353ab720a78971285407e0a
Meriem <152006164+merry122@users.noreply.github.com>
2024-11-24 17:41:01 +0100

    Update Task.java

--- src/mini/project/Task.java
+++ src/mini/project/Task.java
@@ -1,20 +1,39 @@
 package mini.project;
-
+/**
+ *@author meriem harrouz 
+ */
 public interface Task {
 	
-    // Method to create task details
+    /**
+     *  Method to create task details
+     * @param name
+     * @param description
+     * @param dueDate
+     */
     void createTask(String name,String description,String dueDate);
 
-    // Method to edit task details
+    /**
+     *  Method to edit task details
+     * @param newName
+     * @param newDescription
+     * @param newDueDate
+     */
     void editTask(String newName,String newDescription,String newDueDate);
 
-    // Method to delete the task
+    /**
+     *  Method to delete the task
+     */
     void deleteTask();
 
-    // Method to mark the task as completed
+    /**
+     *  Method to mark the task as completed
+     */
     void markAsCompleted();
 
-    // Method to display task details (optional, for easier debugging)
+    /**
+     *  Method to display task details (optional, for easier debugging)
+     * @return
+     */
     String getTaskDetails();
 }
 
