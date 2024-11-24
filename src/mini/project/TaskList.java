7cbc352ecf5159d9e83cac0c9f9c87389b45f453
Raouane <162448387+Raouane06@users.noreply.github.com>
2024-11-24 22:16:29 +0100

    Update TaskList.java

--- src/mini/project/TaskList.java
+++ src/mini/project/TaskList.java
@@ -9,34 +9,18 @@ public abstract class TaskList {
 	/**
 	 *  it's a class that manages tasks
 	 */
-    
-    protected List<Task> tasks=new ArrayList<>();
-
     /**
      *  Method to add a task to the list
      * @param task attribute of type Task
      */
-    public void addTask(Task task) {
-        tasks.add(task);
-    }
+    public abstract addTask(Task task);
 
     /**
      *  Method to remove a task from the list
      * @param task attribute of type Task
      */
-    public void deleteTask(Task task) {
-        tasks.remove(task);
-    }
+    public abstract deleteTask(Task task);
 
-    /**
-     *  Method to get the task at a specific index
-     * @param index  the index of the task we need to find
-     * @return the task that was found according to the index
-     */
-    public Task getTask(int index) {
-        if (index>=0&&index<tasks.size())    return tasks.get(index);
-        else    throw new IndexOutOfBoundsException("Invalid task index");
-    }
 
     /**
      *  Method to edit a task in the list
@@ -45,10 +29,7 @@ public abstract class TaskList {
      * @param newDescription
      * @param newDueDate
      */
-    public void editTask(int index,String newName,String newDescription,String newDueDate) {
-        Task task=getTask(index);
-        task.editTask(newName,newDescription,newDueDate);
-    }
+    public abstract editTask(int index,String newName,String newDescription,String newDueDate);
 
     /**
      *  Abstract method to display all tasks
