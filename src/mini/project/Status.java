15a5ba3cf68d8a5c22cadef37c0c40e8d5532284
Meriem <152006164+merry122@users.noreply.github.com>
2024-11-25 00:06:59 +0100

    Create Status.java

---
+++ src/mini/project/Status.java
@@ -0,0 +1,14 @@
+package mini.project;
+
+/**
+ * @author meriem harrouz
+ * Enum representing task status.
+ * Provides clear status options: PENDING, IN_PROGRESS, COMPLETED.
+ */
+public enum Status {
+
+	PENDING,
+    IN_PROGRESS,
+    COMPLETED
+    
+}
