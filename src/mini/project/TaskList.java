67a1df1631c3779f8c729fef75a54300cdc5dd38
Meriem <152006164+merry122@users.noreply.github.com>
2024-11-24 17:41:33 +0100

    Update TaskList.java

--- src/mini/project/TaskList.java
+++ src/mini/project/TaskList.java
@@ -1,35 +1,57 @@
 package mini.project;
-
+/**
+ *@author meriem harrouz 
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class TaskList {
-	
-    // List to store tasks
+	/**
+	 *  it's a class that manages tasks
+	 */
+    
     protected List<Task> tasks=new ArrayList<>();
 
-    // Method to add a task to the list
+    /**
+     *  Method to add a task to the list
+     * @param task
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
-    // Method to remove a task from the list
+    /**
+     *  Method to remove a task from the list
+     * @param task
+     */
     public void deleteTask(Task task) {
         tasks.remove(task);
     }
 
-    // Method to get the task at a specific index
+    /**
+     *  Method to get the task at a specific index
+     * @param index 
+     * @return
+     */
     public Task getTask(int index) {
         if (index>=0&&index<tasks.size())    return tasks.get(index);
         else    throw new IndexOutOfBoundsException("Invalid task index");
     }
 
-    // Method to edit a task in the list
+    /**
+     *  Method to edit a task in the list
+     * @param index
+     * @param newName
+     * @param newDescription
+     * @param newDueDate
+     */
     public void editTask(int index,String newName,String newDescription,String newDueDate) {
         Task task=getTask(index);
         task.editTask(newName,newDescription,newDueDate);
     }
 
-    // Abstract method to display all tasks (implementation depends on the subclass)
+    /**
+     *  Abstract method to display all tasks
+     */
     public abstract void displayTasks();
 }
