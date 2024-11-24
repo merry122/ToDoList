27c05a5e865afa4c8ffc9df5f25a2fb90e034206
Raouane <162448387+Raouane06@users.noreply.github.com>
2024-11-24 21:21:36 +0100

    Create DBConnection.java

---
+++ src/mini/project/DBConnection.java
@@ -0,0 +1,27 @@
+package com.raouane.todolist;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    private static final String Host="127.0.0.1";
+    private static final int PORT=3306;
+    private static final String DB_NAME="to_do_list";
+    private static final String USERNAME="root";
+    private static final String PASSWORD="";
+
+    private static Connection connection;
+
+    public static Connection getConnection(){
+
+        try{
+            connection= DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s",Host,PORT,DB_NAME),USERNAME,PASSWORD);
+        } catch(SQLException mess){
+            mess.printStackTrace();
+        }
+        return connection;
+    }
+
+
+}
