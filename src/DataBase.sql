6334d082fc71db43d13b5f292aefb1fd7dd86f2e
Raouane <162448387+Raouane06@users.noreply.github.com>
2024-11-25 01:20:50 +0100

    Update DataBase.sql

--- src/DataBase.sql
+++ src/DataBase.sql
@@ -5,8 +5,7 @@ CREATE DATABASE to_do_list;
 USE to_do_list;
 
 CREATE TABLE Tasks{
-    id INT (10) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(15),
+    name VARCHAR(15) PRIMARY KEY AUTO_INCREMENT,
     description VARCHAR(200) NOT NULL,
     dueDate Date,
     is_completed BOOLEAN DEFAULT FALSE
