e282813d54035f2bbd5da15f63616a409c4ad448
Meriem <152006164+merry122@users.noreply.github.com>
2024-12-09 06:10:58 +0100

    Update DBconnection.java

--- demo/src/main/java/miniProject/DBconnection.java
+++ demo/src/main/java/miniProject/DBconnection.java
@@ -7,24 +7,24 @@ import java.sql.SQLException;
  *@author raouane krama
  */
 public class DBconnection {
-    private static final String Host = "127.0.0.1";
-    private static final int PORT = 3306;
-    private static final String DB_NAME = "to_do_list";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String Host="127.0.0.1";
+    private static final int PORT=3306;
+    private static final String DB_NAME="to_do_list";
+    private static final String USERNAME="root";
+    private static final String PASSWORD="";
 
     private static Connection connection;
 
-    public static Connection getConnection() {
-        try {
-            // Add useSSL=false to the URL to disable SSL
-            String url = String.format("jdbc:mysql://%s:%d/%s?useSSL=false", Host, PORT, DB_NAME);
-            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
-        } catch (SQLException mess) {
+    public static Connection getConnection(){
+
+        try{
+            connection= DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s",Host,PORT,DB_NAME),USERNAME,PASSWORD);
+        } catch(SQLException mess){
             mess.printStackTrace();
         }
         return connection;
     }
-}
 
+
+}
 
