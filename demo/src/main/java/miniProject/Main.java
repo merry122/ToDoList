810d090e38ed7eea89d14e5be76b620aa941b466
Meriem <152006164+merry122@users.noreply.github.com>
2024-12-09 05:45:15 +0100

    Update Main.java

--- demo/src/main/java/miniProject/Main.java
+++ demo/src/main/java/miniProject/Main.java
@@ -19,6 +19,39 @@ public class Main extends Application {
         primaryStage.setTitle("ToDo Application");
         primaryStage.show();
     }
+package miniProject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private static Stage primaryStage; // Central stage for switching views
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+
+        // Load the initial view
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/miniProject/AddItem.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("ToDo Application");
+        primaryStage.show();
+    }
+
+    public static void switchView(String fxmlFile) throws Exception {
+        // Method to switch views
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlFile));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
 
     public static void switchView(String fxmlFile) throws Exception {
         // Method to switch views
