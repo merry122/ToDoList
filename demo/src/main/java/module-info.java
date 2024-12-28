79623ee8292553834130e55e5f709991a8478a02
Raouane <162448387+Raouane06@users.noreply.github.com>
2024-12-28 03:59:07 +0100

    Delete demo directory

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.miniProject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens miniProject to javafx.fxml;
-    exports miniProject;
-}
